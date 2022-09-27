5306afff18df1ad75da23ae79c48ec3e03395ed5
Joao Vitor Mengatto Gimenes <jvm.gimenes@hotmail.com>
2022-09-27 11:06:37 -0300

    feat: fix tests

--- tests/matematica/test_matematica.py
+++ tests/matematica/test_matematica.py
@@ -5,6 +5,16 @@ def test_somar():
     result = somar(1, 2)
     assert result == 3
 
-def numero_par():
-    result = numero_par(2)
+def test_numero_par():
+    result = numero_par(10)
     assert result == True
+
+def test_somar_pares():
+    first_number = 2
+    sec_number = 4
+    result = numero_par(first_number)
+    assert result == True
+    result = numero_par(sec_number)
+    assert result == True
+    result = somar(first_number, sec_number)
+    assert result == 6
